4d682cfa307f14e47e2eda8036e186663b46e77e
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2010-01-25 21:48:12 +0100

    better participanttable

--- invite_participants.js
+++ invite_participants.js
@@ -2,28 +2,50 @@ var extensiondir='../extensions/dc-net/'
 var pollID = (location.href).split("/");
 pollID = pollID[pollID.length-2];
 
-new Ajax.Request(extensiondir + 'keyserver.cgi?service=listAllKeys',
-  {
-    method:'get',
-    onSuccess: function(transport){
-      var response = transport.responseText.split("\n");
+$("wizzard_navigation").insert({
+	after: "<h2 id='privacy_enhanced'>Invite Participants to Privacy-Enhanced Poll</h2><div id='currentUsers'></div><div id='addUsers'></div>"
+});
 
-			var privacy_enhanced = "<h2>Invite Participants to Privacy-Enhanced Poll</h2>";
-			privacy_enhanced += "<div><select id='addParticipant'>";
-			for (var line = 0; line < response.length;line++){
-				privacy_enhanced += "<option value='" + response[line] + "' id='" + response[line] + "'>fetching user name ...</option>";
-			}
-			
-			privacy_enhanced += "</select><input type='button' value='Invite' onclick='addParticipant();' /></div>";
+new Ajax.Request(extensiondir + 'webservices.cgi?service=getParticipants&pollID=' + pollID,{
+	method: "get",
+	onFailure: function(){ alert('Failed to fetch participant list.') },
+	onSuccess: function(transport){
+		var participants = transport.responseText.split("\n");
 
-			$("wizzard_navigation").insert({ after: privacy_enhanced});
-			for (var line = 0; line < response.length;line++){
-				new Ajax.Updater(response[line],extensiondir + 'keyserver.cgi?service=getName&gpgID=' + response[line] ,{ method:'get'});
-			}
+		participanttable = "<table><tr><th>Name</th></tr>";
+		participants.each(function(participant){
+			participanttable += "<tr><td id='" + participant + "'>fetching user name ...</td></tr>";
+		});
+		participanttable += "</table>";
+		$("currentUsers").update(participanttable);
 
-    },
-    onFailure: function(){ alert('Failed to fetch keys from keyserver.') }
-  });
+		// Add Participants
+		new Ajax.Request(extensiondir + 'keyserver.cgi?service=listAllKeys', {
+			method:'get',
+			onSuccess: function(transport){
+				var allusers = transport.responseText.split("\n");
+
+				var addParticipantsSelect = "<div><select id='addParticipant'>";
+				allusers.each(function(user){
+					if (!participants.include(user)){
+						addParticipantsSelect += "<option value='" + user + "' id='" + user + "'>fetching user name ...</option>";
+					}
+				});
+				
+				addParticipantsSelect += "</select><input type='button' value='Invite' onclick='addParticipant();' /></div>";
+
+				$("addUsers").update(addParticipantsSelect);
+
+				allusers.each(function(user){
+					new Ajax.Updater(user,extensiondir + 'keyserver.cgi?service=getName&gpgID=' + user,{ method:'get'});
+				});
+
+			},
+			onFailure: function(){ alert('Failed to fetch keys from keyserver.') }
+		});
+	}
+});
+
 
 function addParticipant(){
 	new Ajax.Request(extensiondir + 'webservices.cgi', {
