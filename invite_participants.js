338de6b6225b5bb3b4b30e78bdeb3052534c1bd7
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2010-01-25 15:15:17 +0100

    invite partially implemented

---
+++ invite_participants.js
@@ -0,0 +1,35 @@
+function getName(gpgID){
+	var req = new Ajax.Request('/keyserver/',
+			{
+				parameters: {
+					gpgID: gpgID,
+					service: "getName"
+				}
+			}
+		);
+	return req.responseText;
+}
+
+new Ajax.Request('/keyserver/?service=listAllKeys',
+  {
+    method:'get',
+    onSuccess: function(transport){
+      var response = transport.responseText.split("\n");
+
+			var privacy_enhanced = "<h2>Invite Participants to Privacy enhanced Poll</h2>";
+			privacy_enhanced += "<div><select name='invite_delete'>";
+			for (var line = 0; line < response.length;line++){
+				privacy_enhanced += "<option value='" + response[line] + "' id='" + response[line] + "'>fetching user name ...</option>";
+			}
+			
+			privacy_enhanced += "</select><input type='button' value='Invite' /></div>";
+
+			$("wizzard_navigation").insert({ after: privacy_enhanced});
+			for (var line = 0; line < response.length;line++){
+				new Ajax.Updater(response[line],'/keyserver/?service=getName&gpgID=' + response[line] ,{ method:'get'});
+			}
+
+    },
+    onFailure: function(){ alert('Could not fetch keys.') }
+  });
+
---
+++ main.rb
@@ -0,0 +1,25 @@
+############################################################################
+# Copyright 2009,2010 Benjamin Kellermann                                  #
+#                                                                          #
+# This file is part of dudle.                                              #
+#                                                                          #
+# Dudle is free software: you can redistribute it and/or modify it under   #
+# the terms of the GNU Affero General Public License as published by       #
+# the Free Software Foundation, either version 3 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# Dudle is distributed in the hope that it will be useful, but WITHOUT ANY #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public     #
+# License for more details.                                                #
+#                                                                          #
+# You should have received a copy of the GNU Affero General Public License #
+# along with dudle.  If not, see <http://www.gnu.org/licenses/>.           #
+############################################################################
+
+
+case $d.tab
+when "invite_participants.cgi"
+	$d.html.add_head_script("../extensions/dc-net/lib/prototype.js")
+	$d.html.add_script("../extensions/dc-net/invite_participants.js")
+end
