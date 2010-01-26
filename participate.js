4e7cf2702ae979092867ebc42b2546961403b889
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2010-01-27 00:22:29 +0100

    implement vote interface

--- participate.js
+++ participate.js
@@ -24,11 +24,8 @@ $("tablist").insert({ bottom: li});
 var show = false;
 function showLogin(){
 	if (!show){
-	var l = "<form method='post' action='.'>";
-	l += "<table class='settingstable'><tr><td class='label' ><label for='name'>Name:</label></td>";
-	l += "<td><input id='name' type='text' /></td></tr>";
-	l += "<tr><td class='label'><label for='pass'>Password:</label></td><td><input id='pass' type='password' /></td></tr>";
-	l += "<tr id='loginrow'><td></td><td><input type='button' value='login' onclick='login();location.reload();' id='loginbutton' /></td></tr></table></form>";
+	var l = "Secret Key:<br /><textarea id='pass' cols='140' rows='3'></textarea><br />";
+	l += "<input type='button' value='Login' onclick='login();location.reload();' id='loginbutton' />";
 	$("polltitle").insert({before: l});
 	}
 	show = true;
@@ -45,29 +42,88 @@ function fingerprint(pub){
 function login(){
 	switch (786){
 	case 786:
-		var q = new BigInteger("1552518092300708935130918131258481755631334049434514313202351194902966239949102107258669453876591642442910007680288864229150803718918046342632727613031282983744380820890196288509170691316593175367469551763119843371637221007210577919");
+		var dhmod = new BigInteger("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A63A3620FFFFFFFFFFFFFFFF",16);
 		break
 	case 1024:
-		var q = new BigInteger("179769313486231590770839156793787453197860296048756011706444423684197180216158519368947833795864925541502180565485980503646440548199239100050792877003355816639229553136239076508735759914822574862575007425302077447712589550957937778424442426617334727629299387668709205606050270810842907692932019128194");
+		var dhmod = new BigInteger("179769313486231590770839156793787453197860296048756011706444423684197180216158519368947833795864925541502180565485980503646440548199239100050792877003355816639229553136239076508735759914822574862575007425302077447712589550957937778424442426617334727629299387668709205606050270810842907692932019128194");
 		break;
 	case 1536:
-		var q = new BigInteger("2410312426921032588552076022197566074856950548502459942654116941958108831682612228890093858261341614673227141477904012196503648957050582631942730706805009223062734745341073406696246014589361659774041027169249453200378729434170325843778659198143763193776859869524088940195577346119843545301547043747207749969763750084308926339295559968882457872412993810129130294592999947926365264059284647209730384947211681434464714438488520940127459844288859336526896320919633919");
+		var dhmod = new BigInteger("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF",16);
 		break;
 	}
 
-	var n = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",16);
+	var dcmod = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",16);
 	var g = new BigInteger("2");
 	
-	var sec = new BigInteger(b64tohex($F('pass')),16);
-	var pub = g.modPow(sec,q);
-	var fpr = fingerprint(pub.toString());
+	var sec = new BigInteger($F('pass'),16);
+	var pub = g.modPow(sec,dhmod);
 
-	localStorage.setItem("name",$F('name'));
+	var id = "0x" + SHA256_hash(pub.toString(16)).slice(56,64).toUpperCase();
+
 	localStorage.setItem("sec",sec);
-	localStorage.setItem("q",q);
-	localStorage.setItem("n",n);
-	localStorage.setItem("g",g);
 	localStorage.setItem("pub",pub);
-	localStorage.setItem("fingerprint",fpr);
+	localStorage.setItem("g",g);
+	localStorage.setItem("dhmod",dhmod);
+	localStorage.setItem("dcmod",dcmod);
+	localStorage.setItem("id",id);
 }
 
+
+function htmlid(s){
+	return s.gsub(" ","_").gsub(":","_");
+}
+
+var columns;
+new Ajax.Request(extensiondir + 'webservices.cgi?service=getColumns&pollID=' + pollID, {
+	method:'get',
+	onSuccess: function(transport){
+		columns = transport.responseText.split("\n");
+
+		new Ajax.Request(extensiondir + 'webservices.cgi?service=getParticipants&pollID=' + pollID,{
+			method: "get",
+			onFailure: function(){ alert('Failed to fetch participant list.') },
+			onSuccess: function(transport){
+				participants = transport.responseText.split("\n");
+
+				var row = "";
+				participants.each(function(participant){
+					row += "<tr class='participantrow' id='participant_" + participant + "'>"
+					row += "<td class='name' id='" + participant + "'>Fetching Name for " + participant + "...</td>"
+					row += "<td class='undecided' colspan='" + columns.length + "'>Participant has not voted yet.</td>"
+					row += "</tr>"
+				});
+				$("separator").insert({ before: row });
+
+				// participate
+				if (participants.indexOf(localStorage.getItem("id")) != -1) {
+					var id = localStorage.getItem("id");
+					participaterow = "<td id='"+id+"' class='name'>fetching name for id " + id + "...</td>";
+
+					columns.each(function(col){
+						participaterow += "<td title='"+col+"' class='checkboxes'>";
+						participaterow += "<input id='"+htmlid(col)+"' type='checkbox'/></td>";
+					});
+					participaterow += "<td id='submit' class='date'><input onclick='vote();' type='button' value='Save'></td>";
+
+
+					$("add_participant").update("");
+					$("participant_" + id).update(participaterow);
+				}
+
+				// give everything humanreadable names
+				participants.each(function(participant){
+					new Ajax.Updater(participant,extensiondir + 'keyserver.cgi?service=getName&gpgID=' + participant,{ method:'get'});
+				});
+		}});
+}});
+
+function vote(){
+	var votev = new Object();
+	columns.each(function(col){
+		votev[col] = $(htmlid(col)).checked ? BigInteger.ONE : BigInteger.ZERO;
+	});
+	for (var v in votev){
+		document.write(votev[v]);
+	}
+	
+}
