9859a8d512afff7ad735b5253d993bf58d9deb38
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2010-01-26 14:51:52 +0100

    include header

--- participate.js
+++ participate.js
@@ -1,3 +1,22 @@
+/****************************************************************************
+ * Copyright 2009,2010 Benjamin Kellermann                                  *
+ *                                                                          *
+ * This file is part of dudle.                                              *
+ *                                                                          *
+ * Dudle is free software: you can redistribute it and*or modify it under   *
+ * the terms of the GNU Affero General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or        *
+ * (at your option) any later version.                                      *
+ *                                                                          *
+ * Dudle is distributed in the hope that it will be useful, but WITHOUT ANY *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public     *
+ * License for more details.                                                *
+ *                                                                          *
+ * You should have received a copy of the GNU Affero General Public License *
+ * along with dudle.  If not, see <http:**www.gnu.org*licenses*>.           *
+ ***************************************************************************/
+
 var li = "<li class='nonactive_tab'><a href='javascript:showLogin();'>&nbsp;Login&nbsp;</a></li>"
 li += "<li class='nonactive_tab'><a href='javascript:showRegister();'>&nbsp;Register&nbsp;</a></li>"
 $("tablist").insert({ bottom: li});
