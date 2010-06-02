1489d7c6eecd9b084db9eb7053e497131b4cc38d
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2010-06-02 14:52:54 +0200

    add summary after last register step

--- register.js
+++ register.js
@@ -30,6 +30,7 @@ showRegisterTab();
 var gActiveTabInnerHTML = $('active_tab').innerHTML;
 var gContent = $('content').innerHTML;
 
+// return to last visible tab
 function showContent() {
 	$('content').update(gContent);
 	$('active_tab').removeClassName("nonactive_tab");
@@ -40,31 +41,7 @@ function showContent() {
 	$('active_tab').update(gActiveTabInnerHTML);
 }
 
-function checkUserExistance() {
-	var label, ar;
-	$("registererror").update("");
-	if ($F("name")) {
-		$("next").disable();
-		label = $F("next");
-		$("next").value = _("Checking Username");
-		ar = new Ajax.Request(gsExtensiondir + 'keyserver.cgi', {
-			method: "get",
-			parameters: { service: 'searchId', name: $F("name")},
-			onSuccess: function (transport) {
-				$("next").value = label;
-				$("name").focus();
-				$("registererror").update("<td colspan='2' class='warning'>" + _("A user with the same name already exists.") + "</td>");
-				$("next").enable();
-			},
-			onFailure: function (transport) {
-				$("next").value = label;
-				$("next").enable();
-				secondRegisterStep();
-			}
-		});
-	}
-}
-
+// the first step
 function showRegister(name) {
 	var _r = "<h1>dudle</h1>";
 	_r += "<h2>" + _("Register new Account") + "</h2>";
@@ -96,37 +73,97 @@ function showRegister(name) {
 	} 
 }
 
-function secondRegisterStep() {
+var gsUserName;
+
+// returns all information about the storage of the secret key within a <tr></tr>
+function keyTr() {
 	var _r = "<tr>";
-	_r += "</td><td>";
-	_r += "<td>" + _("Please store the secret key somewhere at your computer (e.&thinsp;g., by copying it to a textfile).") + "</td>";
+	_r += "<td></td>";
+	_r += "<td class='textcolumn'>" + _("Please store the secret key somewhere at your computer (e.&thinsp;g., by copying it to a textfile).") + "</td>";
 	_r += "</tr><tr>";
 	_r += "<td class='label'><label for='key'>" + _("Secret Key:") + "</label></td>";
 	_r += "<td><textarea readonly='readonly' id='key' type='text' cols='100' rows='3'>";
 	_r += goVoteVector.sec.toString(16) + "</textarea></td>";
 	_r += "</tr><tr>";
 	_r += "<td></td>";
-	_r += "<td>" + _("Alternatively, you may bookmark this link, which inserts the key into the login field:");
+	_r += "<td class='textcolumn'>" + _("Alternatively, you may bookmark this link, which inserts the key into the login field:");
 	_r += " <a href=\"javascript:void(";
 	_r += "document.getElementById('key').value='";
 	_r += goVoteVector.sec.toString(16);
 	_r += "')\">";
-	_r += printf(_('insert dudle key (%1)'), [$F('name')]) + '</a>.';
+	_r += printf(_('insert dudle key (%1)'), [gsUserName]) + '</a>.';
 	_r += "</td>";
-	_r += "</tr><tr>";
-	_r += "</td><td>";
-	_r += "<td><input type='button' value='" + _("Previous") + "' onclick='showRegister(\"" + $F('name') + "\");' /> ";
-	_r += "<input type='hidden' id='name' value='" + $F('name') + "' />";
+	_r += "</tr>";
+	return _r;
+}
+
+// the second and last step (asks to store the key)
+function secondRegisterStep() {
+	var _r;
+	gsUserName = $F('name');
+	_r = keyTr();
+	_r += "<tr>";
+	_r += "<td></td>";
+	_r += "<td><input type='button' value='" + _("Previous") + "' onclick='showRegister(\"" + gsUserName + "\");' /> ";
 	_r += "<input type='button' value='" + _("Finish") + "' onclick='register()'/></td>";
 	_r += "</tr>";
 	$('register').update(_r);
 }
 
-function register() {
-	var _name, _pubkey, _ar;
+// avoid double registration
+function checkUserExistance() {
+	var label, ar;
+	$("registererror").update("");
+	if ($F("name")) {
+		$("next").disable();
+		label = $F("next");
+		$("next").value = _("Checking Username");
+		ar = new Ajax.Request(gsExtensiondir + 'keyserver.cgi', {
+			method: "get",
+			parameters: { service: 'searchId', name: $F("name")},
+			onSuccess: function (transport) {
+				$("next").value = label;
+				$("name").focus();
+				$("registererror").update("<td colspan='2' class='warning'>" + _("A user with the same name already exists.") + "</td>");
+				$("next").enable();
+			},
+			onFailure: function (transport) {
+				$("next").value = label;
+				$("next").enable();
+				secondRegisterStep();
+			}
+		});
+	}
+}
 
-	_name = $F('name');
-	_pubkey = "NAME " + _name + "\n";
+
+var gbKeyVisible = false;
+function showKeyAgain() {
+	if (!gbKeyVisible) {
+		gbKeyVisible = true;
+		Element.replace($('keyplaceholder'), "<tr><td class='separator_top' colspan='2'></td></tr>" + keyTr());
+	}
+}
+
+// summary
+function showFinish() {
+	var _r = "<tr>";
+	_r += "<td colspan='2' class='textcolumn'>" + _("You successfully registered an Account. With your secret key, you are able to participate in anonymous polls. Make sure, that you do not loose your secret key. Without the key, nobody is able to vote for the username.") + "</td>";
+	_r += "</tr><tr>";
+	_r += "<td colspan='2'><ul>";
+	_r += "<li><a href='javascript:showKeyAgain()'>" + _('Click here in order to view and backup your secret key (last chance)') + "</a></li>";
+	_r += "<li><a href='javascript:gfReload()'>" + _('Return to dudle home and Schedule a new Poll') + "</a></li>";
+	_r += "</ul></td>";
+	_r += "</tr><tr id='keyplaceholder'></tr>";
+	$('register').update(_r);
+
+}
+
+// send the key to the server
+function register() {
+	var _pubkey, _ar;
+
+	_pubkey = "NAME " + gsUserName + "\n";
 	_pubkey += "DHPUB " + goVoteVector.pub.toString(16);
 	_ar = new Ajax.Request(gsExtensiondir + "keyserver.cgi", {
 		parameters: {service: 'setKey', gpgKey: _pubkey},
@@ -134,7 +171,7 @@ function register() {
 			alert(_("Failed to store key, the server said:") + " " + transport.responseText);
 		},
 		onSuccess: function (transport) {
-			showContent();
+			showFinish();
 		}
 	});
 }
